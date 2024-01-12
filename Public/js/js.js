80c9bdada1a46fa5b9b9eb160a86ccf52522417b
Anna Karolynna <annakarolynnasouza8181@gmail.com>
2024-01-11 23:33:38 -0300

    Terminando de ajustar o html e css do App

--- Public/js/js.js
+++ Public/js/js.js
@@ -1,11 +1,14 @@
-var botaoAddTarefa = document.getElementById("addTarefa").addEventListener("click", formulario);
-var formulario = document.getElementById("formulario");
-var botaoFecharFormulario = document.getElementById("fechar").addEventListener("click",fecharFormulario);
+var botaoAddTarefa = document.getElementById("addTarefa").addEventListener("click", abrirFormulario);
+var botaoFecharFormulario = document.getElementById("fechar").addEventListener("click", fecharFormulario);
 
-function formulario() {
+var formulario = document.getElementById("formulario");
+var overlayF = document.getElementById("overlay");
+function abrirFormulario() {
+    overlay.style.display = "block";
     formulario.style.display = "block";
- }
+}
 
 function fecharFormulario() {
-    formulario.style.display = "none";   
+    formulario.style.display = "none";
+    overlay.style.display = "none";
 }
